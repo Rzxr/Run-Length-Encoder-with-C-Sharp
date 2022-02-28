827eaeccfb188168c77dc4b0232b026336455605
Raz <78645665+rx789@users.noreply.github.com>
2022-02-28 21:27:07 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String enteredString;
+            String compressedString = "";
+            int count = 1;
+
+            Console.WriteLine("Run Length Encoder");
+            Console.WriteLine("Enter a String");
+            enteredString = Console.ReadLine();
+
+            Console.WriteLine("You Entered: " + enteredString);
+            enteredString = enteredString + " ";
+
+            //Compress Using Run Length Encoding 
+
+            for (int i = 0; i < enteredString.Length - 1; i++)
+            {
+                if (enteredString[i] == enteredString[i + 1]) //When same increment counter
+                {      
+                    count++;
+                }
+                else
+                {
+                    compressedString += enteredString[i];
+                    compressedString += count;
+                    count = 1;
+                }
+            }
+            Console.WriteLine("Output" + "\n" + compressedString);
+
+            Console.ReadLine();
+        }
+    }
+}
